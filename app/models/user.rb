71b6b9dec60834923dda4f78fd975bd5f435b6c2
Ruben Sanchez <rfv3@yahoo.com>
2015-03-26 22:13:38 -0600

    added user micropost

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -62,6 +63,10 @@ class User < ActiveRecord::Base
     reset_sent_at < 2.hours.ago
   end
 
+  def feed
+    Micropost.where("user_id = ?", id)
+  end 
+
 private
 
   def downcase_email
@@ -71,7 +76,5 @@ private
   def create_activation_digest 
     self.activation_token = User.new_token
     self.activation_digest = User.digest(activation_token)
-  end    
-
-
+  end   
 end
